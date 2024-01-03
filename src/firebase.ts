cc9f7feb2d42345850e8c1958617a41fe083cfb6
Lee-jeong-wook <126264218+Lee-jeong-wook@users.noreply.github.com>
2024-01-03 23:01:56 +0900

    API 삭제

--- src/firebase.ts
+++ src/firebase.ts
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBpijOaOn5A80cDDVnSm6DC-VwvzBsX7jk",
-  authDomain: "reactsns-v1-6dad0.firebaseapp.com",
-  projectId: "reactsns-v1-6dad0",
-  storageBucket: "reactsns-v1-6dad0.appspot.com",
-  messagingSenderId: "506000159569",
-  appId: "1:506000159569:web:c2b96b67afc98180231970"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
